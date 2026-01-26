9635b5898c6a268c1012dd7bdb6c3ea6a2ad2549
TonyVirues <tonimavir@gmail.com>
2026-01-26 13:21:01 +0100

    start js

--- TODO LIST JQUERY/script.js
+++ TODO LIST JQUERY/script.js
@@ -1,1 +1,31 @@
 alert("esto es una cagada");
+$( document ).ready(function() {
+ 
+    let inputTarea = document.getElementById("inputAñadirTarea");
+    let crearTabla = document.getElementById("tablaTarea");
+
+    
+    function añadirTarea(){
+       let textTarea = inputTarea.value.trim();
+
+    }
+
+
+    function madeClientes() {
+        crearTabla.innerHTML = "";
+            let tr = document.createElement("tr");
+            let textTarea = inputTarea.value.trim();
+
+            tr.innerHTML = `
+            <td>${textTarea}</td>
+            <td>
+                <button onclick="eliminarCliente(${i})" class="btn btn-danger btn-sm">
+                Eliminar
+                </button>
+            </td>
+            `;
+
+            tablaClientes.appendChild(tr);
+            
+    };
+});
