e3e6fcc0e8a0887a39208ff44fe69b2cb1d3a596
TonyVirues <tonimavir@gmail.com>
2026-01-30 13:17:01 +0100

    Datatable

--- Datatable/script.js
+++ Datatable/script.js
@@ -1,1 +1,15 @@
-new DataTable('#example');
+let table = new DataTable('#example');
+ 
+table.on('click', 'tbody tr', function () {
+    let data = table.row(this).data();
+ 
+    alert('You clicked on ' + data[0] + "'s row");
+});
+
+function eventFired(type) {
+    let n = document.querySelector('#demo_info');
+    n.innerHTML +=
+        '<div>' + type + ' event - ' + new Date().getTime() + '</div>';
+    n.scrollTop = n.scrollHeight;
+}
+ 
