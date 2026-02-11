1d5e7565eb2930f5d2c0c4e07d8a554f0d9be073
TonyVirues <tonimavir@gmail.com>
2026-02-11 12:47:50 +0100

    fixed

--- Practica AJAX/script.js
+++ Practica AJAX/script.js
@@ -1,28 +1,29 @@
 console.log("que le ocurre");
 
 //Gate
-let dataArray = []
 let tableBody = document.getElementById("resultado");
 
 fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+    console.log(data)//Eliminar
 
     data.forEach(usuario => {
 
       //Creación de filas.
       let filas = document.createElement("tr");
 
-      //Creación de las columns.
+      //Creación de las diferentes celdas.
       let celdaNombre = document.createElement("td");
       let celdaCorreo = document.createElement("td");
       let celdaCiudad = document.createElement("td");
 
+      //Guardamos el valor en una celda.
       celdaNombre.textContent = usuario.name;
       celdaCorreo.textContent = usuario.email;
       celdaCiudad.textContent = usuario.address.city;
-
+      
+      //Imprimimos las celdas creadas en el html.
       filas.appendChild(celdaNombre);
       filas.appendChild(celdaCorreo);
       filas.appendChild(celdaCiudad);
