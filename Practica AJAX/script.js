568c9cc852c26635bc47410322a760c1f79307b0
TonyVirues <tonimavir@gmail.com>
2026-02-12 14:20:14 +0100

    selectores funcionales

--- Practica AJAX/script.js
+++ Practica AJAX/script.js
@@ -2,13 +2,17 @@ console.log("que le ocurre");
 
 //Gate
 let tableBody = document.getElementById("resultado");
-
-let dataArray = [];
+let selectorOrden = document.getElementById("orden");
 
 //Función que recorre el array de datos e imprime la tabla.
-function  crearTabla(dataArray){
+async function  crearTabla(){
 
-      dataArray.forEach(valor => {
+  let data = await extraerDatos();
+  console.log(data);
+    
+  tableBody.innerHTML ="";
+    data.sort((a,b) => a.name.localeCompare(b.name));
+    data.forEach(valor => {
 
       //Creación de filas.
       let filas = document.createElement("tr");
@@ -32,13 +36,61 @@ function  crearTabla(dataArray){
     });
 }
 
+async function ordenarAscendente(){
+
+    let data = await extraerDatos();
+    console.log(data);
+    tableBody.innerHTML ="";
+
+      data.sort((a,b) => b.name.localeCompare(a.name));
+      data.forEach(valor => {
+
+      //Creación de filas.
+      let filas = document.createElement("tr");
+
+      //Creación de las diferentes celdas.
+      let celdaNombre = document.createElement("td");
+      let celdaCorreo = document.createElement("td");
+      let celdaCiudad = document.createElement("td");
+
+      //Guardamos el valor en una celda.
+      celdaNombre.textContent = valor.name;
+      celdaCorreo.textContent = valor.email;
+      celdaCiudad.textContent = valor.address.city;
+      
+      //Imprimimos las celdas creadas en el html.
+      filas.appendChild(celdaNombre);s
+      filas.appendChild(celdaCorreo);
+      filas.appendChild(celdaCiudad);
+      tableBody.appendChild(filas);
+
+    });
+}
+
+
+/**
+ * EVENTOS.
+ */
+
+//Selector que ordena alfabeticament en orden ascendente.
+selectorOrden.addEventListener("change", () => {
+    if (selectorOrden.value=="asc"){
+        crearTabla();
+    }
+    if (selectorOrden.value =="desc"){
+      ordenarAscendente();
+    }
+});
+
 //Extracción de datos de la Apis.
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(data => {
+async function extraerDatos(){
 
-    console.log(data)//Eliminar.
-    dataArray = data;
-    crearTabla(dataArray);
-
-  });
+  try {
+    let  response = await fetch('https://jsonplaceholder.typicode.com/users');
+    let data = await response.json();
+    
+    return data;
+  } catch (error) {
+    console.error("Error al obtener datos:", error);
+  }
+};
