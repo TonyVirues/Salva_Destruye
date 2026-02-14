d5d72a6d0420040ddc292c80de1f077bc12f5230
Tonyvirues <tonimavir@gmail.com>
2026-02-14 17:59:50 +0100

    Paginación funcional

--- Practica AJAX/script.js
+++ Practica AJAX/script.js
@@ -1,23 +1,34 @@
 console.log("que le ocurre");
-
+// OFFSET = (PAG-1)*LIMIT SACAR PAGINAS PAGES=COUNT/LIMIT Y REDODNEAR HACIA ARRIBA
 //Gate
 let tableBody = document.getElementById("resultado");
 let selectorOrden = document.getElementById("orden");
 let inputBuscarNombre = document.getElementById("inputBusqueda");
 let btnBuscar = document.getElementById("btnBuscar");
+let btnInicio = document.getElementById("principio");
+let btnAtras = document.getElementById("prev");
+let btnSiguiente = document.getElementById("next");
+let btnFin = document.getElementById("ultima");
 
+//Array para almacenar los datos extraídos.
 let dataArray = [];
+
+//Variables de paginación.
+let paginaActual = 1;
+let cantidadPorPagina = 2;
 
 
 
 /**
  * FUNCIONES
  */
+//Funciones de tablas -------->
 
 //Función que inicia la app, pintando la tabla y guardando los datos extraídos.
 async function iniciarApp() {
   dataArray = await extraerDatos();
-  crearTabla(dataArray);
+  // crearTabla(dataArray); //quitamos crear tabla y metemos la funcion de la paginacion
+  paginasContenido();
 }
 
 //Función que pinta la tabla.
@@ -108,6 +119,88 @@ function tablaVacia(){
     
 }
 
+//Funciones de paginación -------->
+
+//Función que devuelve el numero total de páginas disponibles.
+function obtenerPaginasTotales(){
+  return Math.ceil(dataArray.length/cantidadPorPagina);
+}
+
+//Función que devuelve datos de la página deseada.
+function paginasContenido(){
+
+  let pagInicio = (paginaActual-1)*cantidadPorPagina;
+  let pagFin = pagInicio + cantidadPorPagina;
+
+  let imprimirPaginas = dataArray.slice(pagInicio,pagFin);
+
+  crearTabla(imprimirPaginas);
+  gestionBotones();
+}
+
+//Función que valida botones segun la página.
+function gestionBotones(){
+
+  let paginasTotales = obtenerPaginasTotales(); //Cambiar
+
+/**
+ * Condicional para las primeras páginas
+ */
+  //Condicional para que botones de principio y atras, se desactiven en pagina 1.
+  if (paginaActual=== 1){
+    btnInicio.setAttribute("disabled",true);
+    btnAtras.setAttribute("disabled",true);
+  //Condicional para que boton de principio se desactive en pagina 2.
+  }else if(paginaActual===2){
+    btnInicio.setAttribute("disabled",true);
+    btnAtras.removeAttribute("disabled");
+  //Condicional para que ambos botones se activen en el resto de páginas.
+  }else{
+    btnInicio.removeAttribute("disabled");
+    btnAtras.removeAttribute("disabled");   
+  }
+/**
+ * Condicional para las últimas páginas.
+ */
+  //Condicional para que botones de ultimo y siguiente, se desactiven en pagina última.
+  if (paginaActual===paginasTotales){
+    btnFin.setAttribute("disabled",true);
+    btnSiguiente.setAttribute("disabled",true);
+  }else if(paginaActual=== paginasTotales-1){
+    btnFin.setAttribute("disabled",true);
+    btnSiguiente.removeAttribute("disabled");
+  }else{
+    btnFin.removeAttribute("disabled");
+    btnSiguiente.removeAttribute("disabled");
+  }
+}
+//Función que dirige la págiancion a la primera.
+function irPrimera(){
+  paginaActual=1;
+  paginasContenido();
+}
+
+//Función que retrocede una página.
+function pagAnterior(){
+  if (paginaActual>1){
+    paginaActual--;
+    paginasContenido();
+  }
+}
+
+//Función que avanza una página
+function pagSiguiente(){
+  if(paginaActual<obtenerPaginasTotales()){
+    paginaActual++;
+    paginasContenido();
+  }
+}
+//Función que dirige a la última página.
+function irUltima(){
+  paginaActual=obtenerPaginasTotales();
+  paginasContenido();
+}
+
 /**
  * EVENTOS.
  */
@@ -127,9 +220,25 @@ btnBuscar.addEventListener("click", ()=>{
   buscarNombre();
 })
 
+//Boton que dirige a la primera página.
+btnInicio.addEventListener("click",()=>{
+  irPrimera();
+})
+//Boton que actica la funcion "pagAnterior()".
+btnAtras.addEventListener("click",()=>{
+  pagAnterior()
+})
+//Boton que actica la funcion "pagSiguiente()".
+btnSiguiente.addEventListener("click",()=>{
+  pagSiguiente()
+})
+//Boton que actica la funcion " irUltima()".
+btnFin.addEventListener("click",()=>{
+  irUltima()
+})
 
 
-//Función que estrae datos de la Apis.
+//Función que extrae datos de la Apis.
 async function extraerDatos(){
 
   try {
